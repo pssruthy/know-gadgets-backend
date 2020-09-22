830aa9b6b4b16fed7c45721d839c84279a802ae9
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-09-22 11:27:29 +0530

    Added a dummy /api/message url for connect to frontend

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+app.get('/api/message', (req, res) => {
+  res.send(JSON.stringify({ message: 'hai' }));
+});
 
 module.exports = app;
